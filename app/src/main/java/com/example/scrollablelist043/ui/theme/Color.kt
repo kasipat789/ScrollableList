e15c07d4c6148039ad3dcc1eb788671cb82ff1b2
Administrator1 <63410075@go.buu.ac.th>
2022-10-26 20:11:46 +0700

    finished project

---
+++ app/src/main/java/com/example/scrollablelist043/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.example.scrollablelist043.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFA5A5A5)
+val Purple500 = Color(0xFF414141)
+val Purple700 = Color(0xFF2E2E2E)
+val Teal200 = Color(0xFF6F03DA)
